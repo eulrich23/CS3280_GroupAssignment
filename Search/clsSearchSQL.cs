f817bb0c8aabdcddf67f82550ec514140c34345b
eulrich23 <49054538+eulrich23@users.noreply.github.com>
2020-04-17 23:16:10 -0600

    Add files via upload

---
+++ Search/clsSearchSQL.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace Group_Project_Assignment
+{
+    class clsSearchSQL
+    {
+        
+        
+        /// <summary>
+        /// This SQL gets all data on an invoice for a given InvoiceID.
+        /// </summary>
+        /// <param name="sInvoiceID">The InvoiceID for the invoice to retrieve all data.</param>
+        /// <returns>All data for the given invoice.</returns>
+        public string SelectInvoiceNum(string sInvoiceID)
+        {
+            try
+            {
+                string sSQL = "SELECT * FROM Invoices WHERE InvoiceNum = " + sInvoiceID;
+                return sSQL;
+            }
+            catch (Exception ex)
+            {
+                //Just throw the exception
+                throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." +
+                                    MethodInfo.GetCurrentMethod().Name + " -> " + ex.Message);
+            }
+            
+        }
+
+        /// <summary>
+        /// This SQL gets all data on an invoice for a given InvoiceID and InvoiceDate.
+        /// </summary>
+        /// <param name="sInvoiceID"></param>
+        /// <param name="sInvoiceDate"></param>
+        /// <returns>All data for the given invoice.</returns>
+        public string SelectInvoiceNumAndDate(string sInvoiceID, string sInvoiceDate)
+        {
+            try
+            {
+                string sSQL = "SELECT* FROM Invoices WHERE InvoiceNum = " + sInvoiceID + " AND InvoiceDate = #" + sInvoiceDate + "#";
+                return sSQL;
+            }
+            catch (Exception ex)
+            {
+                //Just throw the exception
+                throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." +
+                                    MethodInfo.GetCurrentMethod().Name + " -> " + ex.Message);
+            }
+            
+        }
+        /// <summary>
+        /// This SQL gets all data on an invoice for a given InvoiceID, InvoiceDate, and TotalCost.
+        /// </summary>
+        /// <param name="sInvoiceID"></param>
+        /// <param name="sInvoiceDate"></param>
+        /// <param name="sTotalCost"></param>
+        /// <returns>All data for the given invoice.</returns>
+        public string SelectInvoiceNumAndDateAndCost(string sInvoiceID, string sInvoiceDate, string sTotalCost)
+        {
+            try
+            {
+                string sSQL = "SELECT * FROM Invoices WHERE InvoiceNum = " + sInvoiceID + " AND InvoiceDate = #" + sInvoiceDate + "# AND TotalCost = " + sTotalCost;
+                return sSQL;
+            }
+            catch (Exception ex)
+            {
+                //Just throw the exception
+                throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." +
+                                    MethodInfo.GetCurrentMethod().Name + " -> " + ex.Message);
+            }
+            
+        }
+        /// <summary>
+        /// This SQL gets all data on an invoice for a given TotalCost.
+        /// </summary>
+        /// <param name="sTotalCost"></param>
+        /// <returns>All data for the given invoice.</returns>
+        public string SelectInvoiceCost(string sTotalCost)
+        {
+            try
+            {
+                string sSQL = "SELECT * FROM Invoices WHERE TotalCost = " + sTotalCost;
+                return sSQL;
+            }
+            catch (Exception ex)
+            {
+                //Just throw the exception
+                throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." +
+                                    MethodInfo.GetCurrentMethod().Name + " -> " + ex.Message);
+            }
+            
+        }
+        /// <summary>
+        /// This SQL gets all data on an invoice for a given InvoiceDate, and TotalCost.
+        /// </summary>
+        /// <param name="sInvoiceDate"></param>
+        /// <param name="sTotalCost"></param>
+        /// <returns>All data for the given invoice.</returns>
+        public string SelectInvoiceDateAndCost(string sInvoiceDate, string sTotalCost)
+        {
+            try
+            {
+                string sSQL = "SELECT * FROM Invoices WHERE TotalCost = " + sTotalCost + "and InvoiceDate = #" + sInvoiceDate + "#";
+                return sSQL;
+            }
+            catch (Exception ex)
+            {
+                //Just throw the exception
+                throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." +
+                                    MethodInfo.GetCurrentMethod().Name + " -> " + ex.Message);
+            }
+            
+        }
+        /// <summary>
+        /// This SQL gets all data on an invoice for a given InvoiceDate.
+        /// </summary>
+        /// <param name="sInvoiceDate"></param>
+        /// <returns>All data for the given invoice.</returns>
+        public string SelectInvoiceDate(string sInvoiceDate)
+        {
+            try
+            {
+                string sSQL = "SELECT * FROM Invoices WHERE InvoiceDate = #" + sInvoiceDate + "#";
+                return sSQL;
+            }
+            catch (Exception ex)
+            {
+                //Just throw the exception
+                throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." +
+                                    MethodInfo.GetCurrentMethod().Name + " -> " + ex.Message);
+            }
+            
+        }
+    }
+
+//    - SELECT * FROM Invoices
+//- SELECT* FROM Invoices WHERE InvoiceNum = 5000
+//- SELECT* FROM Invoices WHERE InvoiceNum = 5000 AND InvoiceDate = #4/13/2018#
+//-SELECT * FROM Invoices WHERE InvoiceNum = 5000 AND InvoiceDate = #4/13/2018# AND TotalCost = 120
+//-SELECT * FROM Invoices WHERE TotalCost = 1200
+//- SELECT* FROM Invoices WHERE TotalCost = 1300 and InvoiceDate = #4/13/2018# 
+//-SELECT * FROM Invoices WHERE InvoiceDate = #4/13/2018#
+
+
+}
---
+++ Search/wndSearch.xaml.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Reflection;
+//using namespace CS3280GroupProject.Main
+//using namespace Group_Project_Assignment.Main.
+
+namespace Group_Project_Assignment
+{
+
+    /// <summary>
+    /// This window will be passed in the main window
+    /// </summary>
+    public partial class wndSearch : Window
+    {
+        /// <summary>
+        /// Boolean that indicates whether an invoice has been selected
+        /// </summary>
+        public bool invoiceSelected = false;
+        CS3280GroupProject.Main.wndMain parentWindow; 
+        /// <summary>
+        ///Constructor for the wndSearch Window 
+        /// </summary>
+        public wndSearch(CS3280GroupProject.Main.wndMain parent)
+        {
+            
+            InitializeComponent();
+            parentWindow = parent;
+            clsDataAccess Data = new clsDataAccess();
+            
+           
+            
+        }
+        /// <summary>
+        /// Event Handler for the Invoice Number ComboBox
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void cmbInvoiceNumber_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            try
+            {
+                
+            }
+            catch (Exception ex)
+            {
+                //This is the top level method so we want to handle the exception
+                HandleError(MethodInfo.GetCurrentMethod().DeclaringType.Name,
+                            MethodInfo.GetCurrentMethod().Name, ex.Message);
+            }
+            finally
+            {
+                //This code will always execute
+            }
+        }
+        /// <summary>
+        /// Event Handler for the Invoice Date ComboBox that will update the datagrid appropriately
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void cmbInvoiceDate_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                //This is the top level method so we want to handle the exception
+                HandleError(MethodInfo.GetCurrentMethod().DeclaringType.Name,
+                            MethodInfo.GetCurrentMethod().Name, ex.Message);
+            }
+            finally
+            {
+                //This code will always execute
+            }
+        }
+        /// <summary>
+        /// Event Handler for the Total Charge ComboBox that will update the datagrid appropriately
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void cmbTotalCharge_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            try
+            {
+                
+            }
+            catch (Exception ex)
+            {
+                //This is the top level method so we want to handle the exception
+                HandleError(MethodInfo.GetCurrentMethod().DeclaringType.Name,
+                            MethodInfo.GetCurrentMethod().Name, ex.Message);
+            }
+            finally
+            {
+                //This code will always execute
+            }
+        }
+        /// <summary>
+        /// onClick handler for the Select button that will return the selected invoiceID if one is selected. If not then nothing happens.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnSelect_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                
+                
+                this.Hide();
+                parentWindow.Show();
+            }
+            catch (Exception ex)
+            {
+                //This is the top level method so we want to handle the exception
+                HandleError(MethodInfo.GetCurrentMethod().DeclaringType.Name,
+                            MethodInfo.GetCurrentMethod().Name, ex.Message);
+            }
+            finally
+            {
+                //This code will always execute
+            }
+        }
+        /// <summary>
+        /// An onClick handler that clears the current selection for the Search Window.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnClearSelection_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                
+            }
+            catch (Exception ex)
+            {
+                //This is the top level method so we want to handle the exception
+                HandleError(MethodInfo.GetCurrentMethod().DeclaringType.Name,
+                            MethodInfo.GetCurrentMethod().Name, ex.Message);
+            }
+            finally
+            {
+                //This code will always execute
+            }
+        }
+        /// <summary>
+        /// An onClick handler that hides the Search Window and returns to the Main Window
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                
+                this.Hide();
+                parentWindow.Show();
+            }
+            catch (Exception ex)
+            {
+                //This is the top level method so we want to handle the exception
+                HandleError(MethodInfo.GetCurrentMethod().DeclaringType.Name,
+                            MethodInfo.GetCurrentMethod().Name, ex.Message);
+            }
+            finally
+            {
+                //This code will always execute
+            }
+        }
+        /// <summary>
+        /// An error handling method
+        /// </summary>
+        /// <param name="sClass"></param>
+        /// <param name="sMethod"></param>
+        /// <param name="sMessage"></param>
+        private void HandleError(string sClass, string sMethod, string sMessage)
+        {
+            try
+            {
+                //Would write to a file or database here.
+                MessageBox.Show(sClass + "." + sMethod + " -> " + sMessage);
+            }
+            catch (Exception ex)
+            {
+                System.IO.File.AppendAllText("C:\\Error.txt", Environment.NewLine +
+                                             "HandleError Exception: " + ex.Message);
+            }
+        }
+        /// <summary>
+        /// A method that fills the combo boxes when the Search window is first loaded.
+        /// </summary>
+        private void FillComboBoxes()
+        {
+            try
+            {
+                //cmbInvoiceNumber.
+            }
+            catch (Exception ex)
+            {
+                //Just throw the exception
+                throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." +
+                                    MethodInfo.GetCurrentMethod().Name + " -> " + ex.Message);
+            }
+        }
+        /// <summary>
+        /// An Event Handler that returns to the main window on exit
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void wndSearchWindow_Closed(object sender, EventArgs e)
+        {
+            try
+            {
+
+                this.Hide();
+                parentWindow.Show();
+            }
+            catch (Exception ex)
+            {
+                //This is the top level method so we want to handle the exception
+                HandleError(MethodInfo.GetCurrentMethod().DeclaringType.Name,
+                            MethodInfo.GetCurrentMethod().Name, ex.Message);
+            }
+            finally
+            {
+                //This code will always execute
+            }
+        }
+    }
+}
